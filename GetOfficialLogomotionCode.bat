2af93024cc1a43f59d96555a42c24e97815057ed
Evin Ugur <evinoog96@gmail.com>
2012-11-15 07:42:33 -0500

    Initial Codebase
    Small Batch file to get the official Logomotion code from 2011, and the
    last fully featured Elot codebase that watched for Kinect telemetry.
    This is essentially the base of the project.
    (The code here was written by the Force Team 1073 in 2011 - I am merely
    committing the code, as I am one of many authors of this frc project)

---
+++ GetOfficialLogomotionCode.bat
@@ -0,0 +1,7 @@
+@echo off
+Title Clone our original Logomotion code from 2011
+hg clone https://code.google.com/p/theforceteam-1073.frc11
+echo.
+echo.
+echo The code has been cloned to your working directory
+pause
